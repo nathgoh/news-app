1f740cfe2f4146f900c27ffb51f79c844ae8df16
Nathaniel Goh <gohnathaniel2000@gmail.com>
2024-01-12 18:28:29 -0500

    get preliminary handler working and basic ui

--- news-backend/main.go
+++ news-backend/main.go
@@ -4,15 +4,52 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
-func routes() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "News App")
+func router() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "hello news!",
+		})
 	})
+
+	router.GET("/search", newsSearchHandler)
+
+	router.Run(":" + os.Getenv("PORT"))
+}
+
+func newsSearchHandler(c *gin.Context) {
+	u, err := url.Parse(c.Request.URL.String())
+	if err != nil {
+		http.Error(c.Writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	params := u.Query()
+
+	searchQuery := params.Get("topic")
+	page := params.Get("page")
+	if page == "" {
+		page = "1"
+	}
+
+	fmt.Println("Search Query is: ", searchQuery)
+	fmt.Println("Page is: ", page)
 }
 
 func main() {
-	routes()
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
+	router()
+
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), nil))
 }
--- news-frontend/src/App.tsx
+++ news-frontend/src/App.tsx
@@ -1,14 +1,30 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [searchInput, setSearchInput] = useState<string>("")
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchInput(e.target.value)
+  }
+  
 
   return (
     <>
-      <div>
+      <div className="search-container">
+        <form id="search-form" action="search" method="GET">
+          <input 
+            id="search-bar"
+            name="topic"
+            type="text"
+            placeholder="Search the news regarding..."
+            value={searchInput}
+            onChange={handleChange}
+          />
+          <button id="search-button" type="submit"> Search </button>
+        </form>
+      </div>
+      {/* <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -27,7 +43,7 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+      </p> */}
     </>
   )
 }
