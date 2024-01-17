d199455aeb13b80e979ea8d1acd6bcb17893978e
Nathaniel Goh <gohnathaniel2000@gmail.com>
2024-01-16 23:21:53 -0800

    Add styling

--- news-frontend/src/App.tsx
+++ news-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Article, Results, SearchResults } from './types/News'
-import './App.css'
+import './styles/App.css'
 import axios from 'axios'
 
 function App() {
@@ -44,32 +44,33 @@ function App() {
 
   return (
     <>
+      <h1 id='app-title'> News App </h1>
       <div className="search-container">
         <form id="search-form" action="search" method="GET">
           <input 
             id="search-bar"
             name="topic"
             type="text"
-            placeholder="Search the news regarding..."
+            placeholder="Search for news!"
             value={searchInput}
             onChange={handleChange}
           />
           <button id="search-button" type="submit" onClick={handleOnClick}> Search </button>
         </form>
       </div>
-      <ul className="search-results"> 
+      <li id='article-list'>
         {searchResults?.Results.articles && searchResults.Results.articles.map((article, idx) => (
           <ul className="news-articles" key={`article-${idx}`}>
             <div>
               <a target="_blank" rel="noreferrer noopener" key={`url-${idx}`} href={article.url}>
-                <h3 key={`title-${idx}`}>{article.title}</h3>
+                <h3 id='article-title' key={`title-${idx}`}> {article.title} </h3>
               </a>
-              <p key={`description-${idx}`}>{article.description}</p>            
+              <p id='article-desc' key={`description-${idx}`}> {article.description} </p>            
             </div>
-            <img key={`image-${idx}`} src={article.urlToImage}/>
+            <img id='article-image' key={`image-${idx}`} src={article.urlToImage}/>
           </ul>
         ))}
-      </ul>
+      </li>
     </>
   )
 }
